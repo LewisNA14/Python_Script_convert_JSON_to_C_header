e6bb76570239259446051a6de05f5c0629e9c267
Lewis <lewisnich01@outlook.com>
2026-04-21 12:47:47 +0100

    Finished adding in that the name must start with a string not a int and that special characters cannot be used. Also due to C identifiers special characters such as $, @, #, ?, etc cannot be used.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, Field
 import json
 
 class Register(BaseModel):
     offset: int
-    name: str
+    name: str = Field(pattern="^[A-Za-z_][A-Za-z0-9_]*$")
 
 class Peripheral(BaseModel):
-    name: str
-    base_address: int
-    size: int
+    name: str = Field(...)
+    base_address: int 
+    size: int 
     enabled: bool
     registers: list[Register]
     
@@ -17,8 +17,6 @@ with open("template.JSON") as conf:
     data = json.load(conf)          # Gives a dictionary of the file
 
 periph = Peripheral(**data)
-
-print(periph.name)
 
 print(periph.model_dump())
 
